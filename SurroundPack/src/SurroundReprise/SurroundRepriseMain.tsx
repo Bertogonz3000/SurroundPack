4ebe0c4a967b5636de8a0382dfac0513e2e20a6a
bertogonz3000 <hsga2016@mymail.pomona.edu>
2023-03-30 15:37:03 -0700

    latest

--- SurroundPack/src/SurroundReprise/SurroundRepriseMain.tsx
+++ SurroundPack/src/SurroundReprise/SurroundRepriseMain.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import useLogin from '../hooks/use-login';
 
 import database from '@react-native-firebase/database';
@@ -16,10 +16,10 @@ const SurroundRepriseMain = (authToken: {authToken: string}) => {
 
   const [playing, setPlaying] = useState(false);
   const [songID, setSongID] = useState(0);
-  const [playedTimestamp, setPlayedTimestamp] = useState(0);
   const [songName, setSongName] = useState<string>('loading...');
   const [progress, setProgress] = useState(0);
-
+  const [songURI, setSongURI] = useState('');
+  const [timeOfSet, setTimeOfSet] = useState(0);
 
   database()
     .ref('/session/paused')
@@ -34,23 +34,39 @@ const SurroundRepriseMain = (authToken: {authToken: string}) => {
     });
 
   database()
-    .ref('/session/timestamp')
-    .on('value', snapshot => {
-      setPlayedTimestamp(snapshot.val());
-    });
-
-    database()
     .ref('/session/song_name')
     .on('value', snapshot => {
       setSongName(snapshot.val());
     });
 
-    database()
+  database()
     .ref('/session/progress_ms')
     .on('value', snapshot => {
       setProgress(snapshot.val());
     });
 
+  database()
+    .ref('/session/song_uri')
+    .on('value', snapshot => {
+      setSongURI(snapshot.val());
+    });
+
+  database()
+    .ref('/session/time_of_set')
+    .on('value', snapshot => {
+      setTimeOfSet(snapshot.val());
+    });
+
+  const playTest = useCallback(() => {
+    spotifyApi.play({uris: ['spotify:track:0ujnucfkB1PrSYXWCBS1q0']}).then(
+      function (data) {
+        console.log('played');
+      },
+      function (err) {
+        console.log(err);
+      },
+    );
+  }, []);
 
   const setState = useCallback(() => {
     spotifyApi.getMyCurrentPlaybackState().then(
@@ -58,13 +74,18 @@ const SurroundRepriseMain = (authToken: {authToken: string}) => {
         console.log('succ');
         console.log('paused: ' + data.body.is_playing);
         console.log('here we go: ' + data.body.item.name);
-        database().ref('/session').set({
-          song_name: data.body.item.name ?? 'empty',
-          paused: data.body.is_playing ?? 'empty',
-          song_id: data.body.item.id ?? 'empty',
-          timestamp: data.body.timestamp ?? 'empty',
-          progress_ms: data.body.progress_ms ?? 'empty',
-        });
+        const d = new Date();
+        const newSetTime = d.getTime();
+        database()
+          .ref('/session')
+          .set({
+            song_name: data.body.item.name ?? 'empty',
+            paused: data.body.is_playing ?? 'empty',
+            song_id: data.body.item.id ?? 'empty',
+            progress_ms: data.body.progress_ms ?? 'empty',
+            song_uri: data.body.item.uri ?? 'empty',
+            time_of_set: newSetTime,
+          });
       },
       function (err) {
         console.log(err);
@@ -79,23 +100,59 @@ const SurroundRepriseMain = (authToken: {authToken: string}) => {
     });
   }
 
+  if (isBitch) {
+    spotifyApi.getMyCurrentPlaybackState().then(
+      function (data) {
+        if (!data.body.is_playing && !playing) {
+          const d = new Date();
+          const currentTime = d.getTime();
+          const timeSinceSet = currentTime - timeOfSet;
+          spotifyApi.play({
+            uris: [songURI],
+            position_ms: progress + timeSinceSet + 600,
+          });
+        } else if (data.body.is_playing && playing) {
+          spotifyApi.pause();
+        }
+      },
+      function (err) {
+        console.log(err);
+      },
+    );
+  }
+
   return (
-    <View style={{padding: 50, backgroundColor: '#ffffff'}}>
-      <Text>{'song: ' + songName + ' -- playing: ' + playing + ' -- seconds in: ' + (progress / 1000)}</Text>
+    <View style={{padding: 50, backgroundColor: '#111111'}}>
+      <Text style={styles.textColor}>
+        {'song: ' +
+          songName +
+          ' -- playing: ' +
+          playing +
+          ' -- seconds in: ' +
+          progress / 1000 +
+          ' -- uri: ' +
+          songURI}
+      </Text>
       <Button
         title="hello there!"
         onPress={() => {
           spotifyApi.play();
+          if (!isBitch) {
+            setState();
+          }
         }}
       />
       <Button
         title="General Kenobi!"
         onPress={() => {
           spotifyApi.pause();
+          if (!isBitch) {
+            setState();
+          }
         }}
       />
       <View style={{padding: 40}}>
-        <Text>{'check: ' + isBitch}</Text>
+        <Text style={styles.textColor}>{'check: ' + isBitch}</Text>
         <Button
           title={'Become lead'}
           onPress={() => {
@@ -110,8 +167,11 @@ const SurroundRepriseMain = (authToken: {authToken: string}) => {
         />
       </View>
       <Button title={'Set State'} onPress={() => setState()} />
+      <Button title={'play hardcoded song'} onPress={() => playTest()} />
     </View>
   );
 };
 
 export default SurroundRepriseMain;
+
+const styles = StyleSheet.create({textColor: {color: 'white'}});
